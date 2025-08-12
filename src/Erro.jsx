70d76ebaeb1ab8b24c396a2546ea1af92f80865a
RomasRG <rafaelromagoncalves@gmail.com>
2025-08-12 00:50:41 -0300

    roma5

--- src/Erro.jsx
+++
@@ -1,5 +0,0 @@
-export default function Erro(){
-    return(
-        <>Erro</>
-    )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {HashRouter, Route, Routes,RouterProvider, BrowserRouter, createBrowserRouter} from 'react-router-dom'
-import { createHashRouter } from 'react-router-dom'
 import App from './App.jsx'
-import E from './E.jsx'
-import Erro from './Erro.jsx'
-import Retorno from './Retorno.jsx'
-
-const router = createHashRouter([
-  {path: "/" , element:<App/>},
-  {path:"/E" , element:<E/>},
-  {path:"*" , element:<Erro/>}
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </StrictMode>
 
 )
